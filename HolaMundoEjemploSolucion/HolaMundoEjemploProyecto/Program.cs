f1cd29354656086da68328bb54c4f0602696cfd9
asanchezzdam1 <asanchezzdam1@gmail.com>
2023-07-18 09:24:51 +0200

    Varios archivos subidos

---
+++ HolaMundoEjemploSolucion/HolaMundoEjemploProyecto/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace HolaMundoEjemploProyecto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string? nom = "Juan".ToUpper();
+            nom = nom.ToUpper();
+            nom = null;
+			try
+			{
+                Console.WriteLine("¡Hola, Mundo!".ToUpper() + " " + nom);
+            }
+            catch (Exception ex)
+			{
+                Console.WriteLine("Se ha producido un error: " + ex.Message.ToString());
+                //throw;
+            }
+			
+        }
+    }
+}
