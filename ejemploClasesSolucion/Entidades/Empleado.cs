5386ec36a869ff257f18ea6c5ba3f5eb8b28f240
asanchezzdam1 <asanchezzdam1@gmail.com>
2023-07-18 10:27:17 +0200

    ejecicio de prueba empezado

---
+++ ejemploClasesSolucion/Entidades/Empleado.cs
@@ -0,0 +1,48 @@
+﻿namespace Entidades
+{
+    /*
+        * TIPOS DE CLASES * 
+        * https://learn.microsoft.com/es-es/dotnet/csharp/programming-guide/classes-and-structs/access-modifiers
+        * public - Visible en todo el proyecto
+        * private - No es visible
+        * protected -  
+        * internal - 
+     */
+    public class Empleado
+    {
+        public Empleado()
+        {
+
+        }
+        public Empleado(string nombre, double salario, DateTime fechaAlta, bool alta)
+        {
+            Nombre = nombre;
+            Salario = salario;
+            FechaAlta = fechaAlta;
+            Alta = alta;
+        }
+
+        public string Nombre { get; set; }
+        public double Salario { get; set; }
+        public DateTime FechaAlta { get; }
+        public DateTime FechaDeAlta { get; set; }
+        public bool Alta { get; set; }
+
+        public void calcularAumentoDeSalario()
+        {
+            throw new NotImplementedException();
+        }
+
+        public enum Nivel
+        {
+            normal,
+            bueno,
+            excelente
+        }
+        private Nivel nivel;
+        public void fijarNivel(Nivel nivel)
+        {
+            this.nivel = nivel;
+        }
+    }
+}
---
+++ ejemploClasesSolucion/ejemploClasesProyecto/Program.cs
@@ -0,0 +1,47 @@
+﻿using Entidades;
+using System.Linq.Expressions;
+
+namespace ejemploClasesProyecto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Crear y asignar propiedades mediante la instancia
+            var jose = new Empleado();
+            jose.Nombre = "Jose";
+            jose.FechaDeAlta = new DateTime(2021, 01, 10);
+            jose.Salario = 20000;
+            jose.Alta = true;
+            jose.fijarNivel(Empleado.Nivel.bueno);
+            
+            //Crear y asignar propiedades directamente
+            var maria = new Empleado()
+            {
+                Nombre = "Maria",
+                FechaDeAlta = new DateTime(2022, 03, 01),
+                Salario = 20000,
+                Alta = true
+            };
+            maria.fijarNivel(Empleado.Nivel.bueno);
+
+            //Crear intancias y pasarle parametros al constructor
+            var juan = new Empleado("Juan",
+                                    20000, 
+                                    new DateTime(2023,01,15), 
+                                    true);
+            juan.fijarNivel(Empleado.Nivel.excelente);
+            
+            var lista = new List<Empleado>();
+            lista.Add(jose);
+            lista.Add(maria);
+            lista.Add(juan);
+
+            foreach (var empleado in lista)
+            {
+                empleado.calcularAumentoDeSalario();
+            }
+        }
+    }
+}
